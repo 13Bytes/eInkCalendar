81aff683593335b1aa6fa6f8dff41b7ec48e3d28
Louis <coding@13bytes.de>
2022-02-04 09:49:31 +0100

    remove events from previous day (problem based on time-zones)

--- dataHelper.py
+++ dataHelper.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from datetime import datetime, timezone
 from typing import List
 from urllib.parse import urlparse
@@ -28,14 +29,25 @@ def get_events(max_number: int) -> List[Event]:
     try:
         event_list = events(WEBDAV_CALENDAR_URL, fix_apple=WEBDAV_IS_APPLE)
         event_list.sort(key=sort_by_date)
-        logger.info(
-            "Got {} calendar-entries (capped to {})".format(len(event_list), max_number))
 
+        start_count = 0
         for event in event_list:
             event.start.replace(tzinfo=utc_timezone)
             event.start = event.start.astimezone(current_timezone)
 
-        return event_list[:max_number]
+            # remove events from previous day (problem based on time-zones)
+            day_number = time.localtime().tm_mday
+            event_date = event.start.date()
+            if (day_number == 1 and event_date.month < time.localtime().tm_mon):
+                start_count += 1
+                max_number += 1
+            elif (event_date.day < day_number):
+                start_count += 1
+                max_number += 1
+
+        logger.info("Got {} calendar-entries (capped to {})".format(
+            len(event_list), max_number-start_count))
+        return event_list[start_count:max_number]
 
     except Exception as e:
         logger.critical(e)

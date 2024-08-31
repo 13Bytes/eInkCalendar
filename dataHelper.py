a18e66ef8fa8ba3011c9616aa187583db677913c
João Colaço <4465592+joaocolaco@users.noreply.github.com>
2024-08-31 11:10:12 +0100

    Will filter the dates after the current with icalevents
    icalevents can return events after a start date in UTC format using the start parameter. This avoids problems filtering out the events outside bounds with the day and month number number (that would give problems in a year change).

--- dataHelper.py
+++ dataHelper.py
@@ -25,15 +25,23 @@ def get_events(max_number: int) -> List[Event]:
     logger.info("Retrieving calendar infos")
     utc_timezone = tz.tzutc()
     current_timezone = tz.tzlocal()
-
+    
+    #Current date used to filter events
+    current_UTC_date = datetime.now(timezone.utc)
     try:
         
         #Will store all the events
         event_list = []
         
-        #Get all the events of the provided calendars
+        #Get all the events of the provided calendars, after the current date
         for calendar_settings in WEBDAV_CALENDAR_URLS:
-            calendar_events_list = events(calendar_settings["url"], fix_apple=calendar_settings["is_apple"])
+        
+            calendar_events_list = events(
+                url=calendar_settings["url"], 
+                start=current_UTC_date, 
+                fix_apple=calendar_settings["is_apple"]
+            )
+            
             for event in calendar_events_list:
                 #Add a property called "calendar_name" to the event with the calendar name
                 #Will be used to identify the owner of the event  
@@ -45,24 +53,9 @@ def get_events(max_number: int) -> List[Event]:
         #Sort the list by date
         event_list.sort(key=sort_by_date)
 
-        start_count = 0
-        for event in event_list:
-            event.start.replace(tzinfo=utc_timezone)
-            event.start = event.start.astimezone(current_timezone)
-
-            # remove events from previous day (problem based on time-zones)
-            day_number = time.localtime().tm_mday
-            event_date = event.start.date()
-            if (day_number == 1 and event_date.month < time.localtime().tm_mon):
-                start_count += 1
-                max_number += 1
-            elif (event_date.day < day_number):
-                start_count += 1
-                max_number += 1
-
         logger.info("Got {} calendar-entries (capped to {})".format(
-            len(event_list), max_number-start_count))
-        return event_list[start_count:max_number]
+            len(event_list), max_number))
+        return event_list[0:max_number]
 
     except Exception as e:
         logger.critical(e)
